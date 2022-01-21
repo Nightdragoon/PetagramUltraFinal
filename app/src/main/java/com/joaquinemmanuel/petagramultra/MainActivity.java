b2ea99f17b7bea691eb2685cb30c1e89e39886c0
Nightdragoon <87211704+Nightdragoon@users.noreply.github.com>
2022-01-21 15:56:06 -0600

    Add files via upload

--- app/src/main/java/com/joaquinemmanuel/petagramultra/MainActivity.java
+++ app/src/main/java/com/joaquinemmanuel/petagramultra/MainActivity.java
@@ -1,31 +1,49 @@
 package com.joaquinemmanuel.petagramultra;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.app.Activity;
 import android.content.Intent;
-import android.media.Image;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 
 import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
 
-import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.tabs.TabLayout;
+import com.joaquinemmanuel.petagramultra.Adaptador.AnimalAdaptador;
+import com.joaquinemmanuel.petagramultra.Adaptador.pageAdapter;
+import com.joaquinemmanuel.petagramultra.fragments.Recyclerview_Fragment;
+import com.joaquinemmanuel.petagramultra.fragments.miMascota_Fragment;
+import com.joaquinemmanuel.petagramultra.menu.menu_acercade;
+import com.joaquinemmanuel.petagramultra.menu.menu_contactos;
+import com.joaquinemmanuel.petagramultra.pojo.Animal;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    private RecyclerView listaAnimales;
-    private ArrayList<Animal> animal;
-
-
     private ImageButton imgButton;
+
+    private Toolbar toolbar;
+    String user;
+
+
+
+
+
+
+    private TabLayout tabLayout;
+
+    private ViewPager viewPager;
+
 
 
 
@@ -37,22 +55,38 @@ public class MainActivity extends AppCompatActivity {
         Toolbar miActionBar = findViewById(R.id.tbToolbar);
         setSupportActionBar(miActionBar);
 
+
+        viewPager = findViewById(R.id.viewPager);
+        tabLayout = findViewById(R.id.tablayout);
+        toolbar = findViewById(R.id.tbappbar);
         imgButton = findViewById(R.id.imgButton);
 
+        imgButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this , MainActivity2.class);
+                startActivity(intent);
+                finish();
+            }
+        });
 
+        Bundle parametros = getIntent().getExtras();
+        user = parametros.getString("A");
 
+        setUpViewpager(user);
 
+        if(toolbar != null) {
+            setSupportActionBar(toolbar);
+        }
 
-        animal = new ArrayList<Animal>();
-        listaAnimales = findViewById(R.id.rvMascota);
-        LinearLayoutManager glm = new LinearLayoutManager(this);
-        glm.setOrientation(RecyclerView.VERTICAL);
+    }
 
+    private void setUpViewpager(String user) {
+        viewPager.setAdapter(new pageAdapter(getSupportFragmentManager() , agregarFragment(user)));
+        tabLayout.setupWithViewPager(viewPager);
+        tabLayout.getTabAt(0).setIcon(R.drawable.casa);
+        tabLayout.getTabAt(1).setIcon(R.drawable.dragonsito);
 
-        listaAnimales.setLayoutManager(glm);
-
-        inicializarAnimales();
-        inicializarListaAnimales();
 
 
 
@@ -60,23 +94,54 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void inicializarAnimales(){
-        animal.add(new Animal(R.drawable.chimuelo , getResources().getString(R.string.Chimuelo)));
-        animal.add(new Animal(R.drawable.spyro , getResources().getString(R.string.Spyro)));
-        animal.add(new Animal(R.drawable.kali , getResources().getString(R.string.Spyro)));
-
-    }
-    public void inicializarListaAnimales(){
-        AnimalAdaptador adaptador = new AnimalAdaptador(MainActivity.this , animal , imgButton);
-        listaAnimales.setAdapter(adaptador);
-
-
-
+    private ArrayList<Fragment> agregarFragment(String user) {
+        ArrayList<Fragment> fragments = new ArrayList<>();
+        fragments.add(new Recyclerview_Fragment(user));
+        fragments.add(new miMascota_Fragment(user));
+        return fragments;
     }
 
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_opciones , menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.mContacto:
+                Intent intent = new Intent(MainActivity.this, menu_contactos.class );
+                startActivity(intent);
+                break;
+
+            case R.id.mAcerDe:
+                Intent intent2 = new Intent(MainActivity.this , menu_acercade.class);
+                startActivity(intent2);
+                break;
+            case R.id.mCambiarCuenta:
+                Intent intent3 = new Intent(MainActivity.this , Datos.class);
+                startActivity(intent3);
+                finish();
+                break;
+        }
+
+
+
+
+
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public String getUser(){
+
+        return user;
 
     }
+}
 
 
 
--- app/src/main/java/com/joaquinemmanuel/petagramultra/MainActivity2.java
+++ app/src/main/java/com/joaquinemmanuel/petagramultra/MainActivity2.java
@@ -1,21 +1,27 @@
 package com.joaquinemmanuel.petagramultra;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.appcompat.widget.Toolbar;
+
+import com.joaquinemmanuel.petagramultra.Adaptador.FavoritoAdaptador;
+import com.joaquinemmanuel.petagramultra.db.DB;
+import com.joaquinemmanuel.petagramultra.pojo.Animal;
 
 import java.util.ArrayList;
 
 public class MainActivity2 extends AppCompatActivity {
     private ArrayList<Animal> favorito;
+    private ArrayList<Animal> prelist;
     private RecyclerView listafavorito;
     private ImageView PerraFoto;
     private TextView PerroNombre;
@@ -30,48 +36,36 @@ public class MainActivity2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
         Toolbar miActionBar2 = findViewById(R.id.tbToolbar2);
+        listafavorito = findViewById(R.id.rcFavoritos);
         setSupportActionBar(miActionBar2);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        favorito = new ArrayList<Animal>();
 
-        Bundle parametros = getIntent().getExtras();
-        String perroNombre = parametros.getString("PerroNombre");
-        int perraFoto = parametros.getInt("PerraFoto");
-        int perrosLikes = parametros.getInt("PerrosLikes");
-
-        listafavorito = findViewById(R.id.rcFavoritos);
-        LinearLayoutManager glm = new LinearLayoutManager(this);
-        glm.setOrientation(RecyclerView.VERTICAL);
-        listafavorito.setLayoutManager(glm);
-
-        favorito.add(new Animal(perraFoto , perroNombre));
-
-
+        getFav();
         inicializarListaAnimales();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        linerLayoutManager();
 
 
     }
-    public void inicializarListaAnimales(){
-        FavoritoAdaptador adaptador = new FavoritoAdaptador(MainActivity2.this , favorito);
+    public void inicializarListaAnimales() {
+        FavoritoAdaptador adaptador = new FavoritoAdaptador(MainActivity2.this, favorito);
         listafavorito.setAdapter(adaptador);
+    }
+
+    public void getFav(){
+        DB db = new DB(MainActivity2.this);
+        favorito = db.obtenerFavoritos();
+
+    }
+
+    public void linerLayoutManager(){
+        LinearLayoutManager glm = new LinearLayoutManager(MainActivity2.this);
+        glm.setOrientation(RecyclerView.VERTICAL);
+        listafavorito.setLayoutManager(glm);
+
+    }
+
 
 }
 
 
-}
